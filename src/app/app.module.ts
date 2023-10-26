c3f48612bceab615ceab0c1a012f1dc040f830fb
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-10-27 00:08:41 +0800

    - installing tailwind and configure it. - Add component header and configure the header - to implement tailwind please npm install/update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  // it will be called later. See header.component.html
+  logoImage: any;
+  logoSquare: any;
+  urlLogo: any;
+
+  constructor() {
+    this.logoImage = '/assets/img/logo-landscape.png';
+    this.logoSquare = 'assets/img/favicon.png';
+    this.urlLogo = '';
+  }
+}
