122a858d25c79a9d837e2aed5376ceacd5aed159
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-10-27 15:33:11 +0800

    - edit header component - add footer component note: next, need configure in mobile aspect ratio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LandingpageComponent } from './landingpage/landingpage.component';
+import { HeaderComponent } from './landing-page/header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeroComponent } from './landing-page/hero/hero.component';
+import { FooterComponent } from './landing-page/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LandingpageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, LandingPageComponent, HeroComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/landing-page/footer/footer.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+})
+export class FooterComponent {
+  logoFooter: any;
+  year: any;
+  companyName: any;
+  isFacebookActive: boolean;
+  isInstagramActive: boolean;
+  isTwitterActive: boolean;
+  isGitHubActive: boolean;
+  isDribbleActive: boolean;
+  facebookLink: any;
+  instagramLink: any;
+  twitterLink: any;
+  gitHubLink: any;
+  dribbleLink: any;
+
+  constructor() {
+    this.logoFooter = 'assets/img/logo-landscape.png';
+    this.year = new Date().getFullYear();
+    this.companyName = 'PT Artha Boga Sejahtera';
+
+    // Social Media Contoller:
+    // Facebook
+    this.isFacebookActive = true;
+    this.facebookLink = '#';
+    // Instagram
+    this.isInstagramActive = true;
+    this.instagramLink = '#';
+    // Twitter
+    this.isTwitterActive = true;
+    this.twitterLink = '#';
+    // GitHub
+    this.isGitHubActive = true;
+    this.gitHubLink = '#';
+    // Dribble
+    this.isDribbleActive = false;
+    this.dribbleLink = '#';
+  }
+}
