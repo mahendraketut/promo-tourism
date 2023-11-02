e0ce1cffc286e1f1b070d38017a1a9bb761a0883
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-11-03 01:50:04 +0800

    styling auth pg (login and register) modify route

--- src/app/auth-page/register/register.component.ts
+++ src/app/auth-page/register/register.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  logo: any;
 
+  constructor() {
+    this.logo = '/assets/img/logo-landscape.png';
+  }
 }
