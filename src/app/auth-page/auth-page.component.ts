bf743f8f68146d49581977c8a8648c73558ca4ed
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-11-04 01:23:12 +0800

    update auth page

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
-  styleUrls: ['./auth-page.component.css']
+  styleUrls: ['./auth-page.component.css'],
 })
 export class AuthPageComponent {
+  cover: any;
 
+  constructor() {
+    this.cover = '/assets/img/imagecover3.jpg';
+  }
 }
