16a08255049c7342195b2553828113791b75d5bd
hendryar <115210134+hendryar@users.noreply.github.com>
2023-11-09 02:13:45 +0800

    passw bcrypt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,24 +1,35 @@
 import User from '../models/User.js';
 import Role from '../models/Role.js';
+import bcrypt from 'bcryptjs';
+import { response } from 'express';
 
 
+//TODO: register error handling perlu diperbaikin, biar bisa di acc front end.
 export const register = async (req, res, next) => {
     try {
         console.log("nyari user");
-        const role = await Role.find({role: 'User'});
-        console.log("masuk register");
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            roles: role.map(role => role._id),
-            address: req.body.address,
-            isAdmin: false,
-            
-        });
-        await newUser.save();
-        return res.status(200).send("User registered successfully!");
+        //cari user udah ada apa belom
+        const doesEmailExist = await User.findOne({ email: req.body.email});
+        if (doesEmailExist) {
+        return res.status(400).send("Email already exists");
+        }
+        else{
+            const role = await Role.find({role: 'User'});
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword,
+                roles: role.map(role => role._id),
+                address: req.body.address,
+                isAdmin: false,
+                
+            });
+            await newUser.save();
+            return res.status(200).send("User registered successfully!");
+        }
     } catch (error) {
-        console.log(error);
+        return res.status(500).send(error);
     }
 };
