83e20551354ab4a3a6a30e5d2dcb45336f5cd005
hendryar <115210134+hendryar@users.noreply.github.com>
2023-11-16 20:53:40 +0800

    login backend working with jwt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,6 +6,8 @@ import { CreateError } from '../utils/error.js';
 import { CreateSuccess } from '../utils/success.js';
 
 
+import jwt from 'jsonwebtoken';
+
 //TODO: register error handling perlu diperbaikin, biar bisa di acc front end.
 export const register = async (req, res, next) => {
     try {
@@ -41,9 +43,15 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
     //process the login
     try {
-        const user = await User.findOne({email: req.body.email});
+        console.log("masuk login");
+        //this also fetches the roles
+        const user = await User.findOne({email: req.body.email}).populate("roles", "role");
+        const {roles} = user;
+        console.log("nyari user sama roles");
+        console.log(user);
+        console.log(roles);
+        console.log("sukses nyari role sama user");
         if (!user) {
-            // return res.status(400).send("Email is not found");
             return next(CreateError(400, "Email is not found"));
         }
         const validPassword = await bcrypt.compare(req.body.password, user.password);
@@ -51,8 +59,20 @@ export const login = async (req, res, next) => {
             // return res.status(400).send("Invalid password");
             return next(CreateError(400, "Invalid password"));
         }
-        // return res.status(200).send("Logged in successfully!");
-        return next(CreateSuccess(200, "Logged in successfully!"));
+        console.log("psw valid");
+        
+        const token = jwt.sign({id: user._id, roles:roles, isAdmin: user.isAdmin}, 
+            process.env.TOKEN_SECRET);
+        console.log("token created");
+        res.cookie("access_token", token, {
+            httpOnly: true} ).status(200).json({
+                message: "Logged in successfully!",
+                token: token,
+                isAdmin: user.isAdmin,
+                roles: roles,
+                data:user,
+            });
+        // return next(CreateSuccess(200, "Logged in successfully!"));
 
     } catch (error) {
         // return res.status(500).send(error);
