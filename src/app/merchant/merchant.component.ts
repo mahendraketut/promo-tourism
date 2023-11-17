f4805094f17537d550caca05089cbc9c11a9973a
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-11-17 23:12:39 +0800

    merchant registration

--- src/app/merchant/merchant.component.ts
+++ src/app/merchant/merchant.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-merchant',
   templateUrl: './merchant.component.html',
-  styleUrls: ['./merchant.component.css']
+  styleUrls: ['./merchant.component.css'],
 })
 export class MerchantComponent {
-
+  image: any = 'assets/img/imagecover.jpg';
+  tourismMalaysiaLogo: any = 'assets/img/tourismMalaysia.png';
+  promoTourismLogo: any = 'assets/img/logo-landscape.png';
+  userRole: any = 'na';
 }
