9895a45bb1b15ef8cc9664b6579965441adc8a4d
I Ketut Mahendra <63593289+mahendraketut@users.noreply.github.com>
2023-11-29 23:49:05 +0800

    all blank page --> finished

--- src/app/merchant/admin-merchant/order-list/order-list.component.ts
+++ src/app/merchant/admin-merchant/order-list/order-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.css']
+  styleUrls: ['./order-list.component.css'],
 })
-export class OrderListComponent {
-
+export class OrderListComponent implements OnInit {
+  dtOptions: any = {};
+  ngOnInit(): void {
+    //the datatable
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 5,
+      processing: true,
+      lengthMenu: [5, 15, 5, 5, 5, 10, 5],
+      responsive: true,
+      dom: 'Bfrtip',
+      buttons: ['copy', 'print', 'excel', 'pdf'],
+    };
+  }
 }
