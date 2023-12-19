902b3e254bd532e3b079f102972936434f0fd27a
hendryar <115210134+hendryar@users.noreply.github.com>
2023-12-20 02:28:39 +0800

    merchant-review update

--- src/app/officer/admin-officer/merchant-review/merchant-review.component.ts
+++ src/app/officer/admin-officer/merchant-review/merchant-review.component.ts
@@ -1,6 +1,176 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { AuthService } from 'src/app/services/auth.service';
+// import Swal from 'sweetalert2';
+
+
+// @Component({
+//   selector: 'app-merchant-review',
+//   templateUrl: './merchant-review.component.html',
+//   styleUrls: ['./merchant-review.component.css'],
+// })
+// export class MerchantReviewComponent implements OnInit {
+//   dtOptions: any = {};
+//   merchants: any[] = [];
+
+//   constructor(private authService: AuthService) {}
+
+//   ngOnInit(): void {
+//     this.dtOptions = {
+//       pagingType: 'full_numbers',
+//       pageLength: 5,
+//       processing: true,
+//       lengthMenu: [5, 15, 5, 5, 5, 10, 10],
+//       responsive: true,
+//       dom: 'Bfrtip',
+//       buttons: ['copy', 'print', 'excel', 'pdf'],
+//     };
+//     this.fetchMerchants();
+//   }
+
+//   fetchMerchants() {
+//     this.authService.getMerchants().subscribe({
+//       next: (data) => {
+//         //this enters the merchants data into our local array.
+//         this.merchants = data;
+//       },
+//       error: (error) => {
+//         console.error('Error fetching merchants:', error);
+//       },
+//       complete: () => {
+//         console.log('Merchant data retrieval complete.');
+//       }
+//     });
+//   }
+
+
+//   // acceptMerchant(merchantId: string) {
+//   //   this.authService.acceptMerchant(merchantId).subscribe({
+//   //     next: (response) => {
+//   //       console.log('Merchant accepted:', response);
+//   //     },
+//   //     error: (error) => {
+//   //       Swal.fire({
+//   //         icon: 'error',
+//   //         title: 'Oops...',
+//   //         text: 'Something went wrong!',
+//   //       });
+//   //     },
+//   //     complete: () => {
+//   //       console.log('Accepting merchant complete.');
+//   //       Swal.fire({
+//   //         icon: 'success',
+//   //         title: 'Merchant accepted!',
+//   //         text: 'Merchant has been accepted.',
+//   //       }).then(() => {
+//   //         // Reload the page
+//   //         window.location.reload();
+//   //       });
+//   //     }
+//   //   });
+//   // }
+
+//   // rejectMerchant(merchantId: string) {
+//   //   this.authService.rejectMerchant(merchantId).subscribe({
+//   //     next: (response) => {
+//   //       console.log('Merchant rejected:', response);
+//   //     },
+//   //     error: (error) => {
+//   //       Swal.fire({
+//   //         icon: 'error',
+//   //         title: 'Oops...',
+//   //         text: 'Something went wrong!',
+//   //       });
+//   //     },
+//   //     complete: () => {
+//   //       console.log('Rejecting merchant complete.');
+//   //       Swal.fire({
+//   //         icon: 'success',
+//   //         title: 'Merchant rejected!',
+//   //         text: 'Merchant has been rejected.',
+//   //       }).then(() => {
+//   //         // Reload the page
+//   //         window.location.reload();
+//   //       });
+//   //     }
+//   //   });
+//   // }
+
+
+
+//   acceptMerchant(merchantId: string) {
+//     Swal.fire({
+//       title: 'Are you sure?',
+//       text: "Do you want to accept this merchant?",
+//       icon: 'warning',
+//       showCancelButton: true,
+//       confirmButtonColor: '#3085d6',
+//       cancelButtonColor: '#d33',
+//       confirmButtonText: 'Yes, accept it!'
+//     }).then((result) => {
+//       if (result.isConfirmed) {
+//         this.authService.acceptMerchant(merchantId).subscribe({
+//           next: (response) => {
+//             console.log('Merchant accepted:', response);
+//             Swal.fire(
+//               'Accepted!',
+//               'The merchant has been accepted.',
+//               'success'
+//             ).then(() => {
+//               window.location.reload();
+//             });
+//           },
+//           error: (error) => {
+//             Swal.fire({
+//               icon: 'error',
+//               title: 'Oops...',
+//               text: 'Something went wrong!',
+//             });
+//           }
+//         });
+//       }
+//     });
+//   }
+  
+//   rejectMerchant(merchantId: string) {
+//     Swal.fire({
+//       title: 'Are you sure?',
+//       text: "Do you want to reject this merchant?",
+//       icon: 'warning',
+//       showCancelButton: true,
+//       confirmButtonColor: '#d33',
+//       cancelButtonColor: '#3085d6',
+//       confirmButtonText: 'Yes, reject it!'
+//     }).then((result) => {
+//       if (result.isConfirmed) {
+//         this.authService.rejectMerchant(merchantId).subscribe({
+//           next: (response) => {
+//             console.log('Merchant rejected:', response);
+//             Swal.fire(
+//               'Rejected!',
+//               'The merchant has been rejected.',
+//               'success'
+//             ).then(() => {
+//               window.location.reload();
+//             });
+//           },
+//           error: (error) => {
+//             Swal.fire({
+//               icon: 'error',
+//               title: 'Oops...',
+//               text: 'Something went wrong!',
+//             });
+//           }
+//         });
+//       }
+//     });
+//   }
+  
+// }
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -11,6 +181,9 @@ import Swal from 'sweetalert2';
 export class MerchantReviewComponent implements OnInit {
   dtOptions: any = {};
   merchants: any[] = [];
+  @ViewChild(DataTableDirective, {static: false})
+  datatableElement: DataTableDirective
+  dtTrigger: Subject<any> = new Subject<any>();
 
   constructor(private authService: AuthService) {}
 
@@ -26,12 +199,17 @@ export class MerchantReviewComponent implements OnInit {
     };
     this.fetchMerchants();
   }
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
 
   fetchMerchants() {
     this.authService.getMerchants().subscribe({
       next: (data) => {
         //this enters the merchants data into our local array.
         this.merchants = data;
+        this.dtTrigger.next(null as any);
       },
       error: (error) => {
         console.error('Error fetching merchants:', error);
@@ -43,126 +221,56 @@ export class MerchantReviewComponent implements OnInit {
   }
 
 
-  // acceptMerchant(merchantId: string) {
-  //   this.authService.acceptMerchant(merchantId).subscribe({
-  //     next: (response) => {
-  //       console.log('Merchant accepted:', response);
-  //     },
-  //     error: (error) => {
-  //       Swal.fire({
-  //         icon: 'error',
-  //         title: 'Oops...',
-  //         text: 'Something went wrong!',
-  //       });
-  //     },
-  //     complete: () => {
-  //       console.log('Accepting merchant complete.');
-  //       Swal.fire({
-  //         icon: 'success',
-  //         title: 'Merchant accepted!',
-  //         text: 'Merchant has been accepted.',
-  //       }).then(() => {
-  //         // Reload the page
-  //         window.location.reload();
-  //       });
-  //     }
-  //   });
-  // }
-
-  // rejectMerchant(merchantId: string) {
-  //   this.authService.rejectMerchant(merchantId).subscribe({
-  //     next: (response) => {
-  //       console.log('Merchant rejected:', response);
-  //     },
-  //     error: (error) => {
-  //       Swal.fire({
-  //         icon: 'error',
-  //         title: 'Oops...',
-  //         text: 'Something went wrong!',
-  //       });
-  //     },
-  //     complete: () => {
-  //       console.log('Rejecting merchant complete.');
-  //       Swal.fire({
-  //         icon: 'success',
-  //         title: 'Merchant rejected!',
-  //         text: 'Merchant has been rejected.',
-  //       }).then(() => {
-  //         // Reload the page
-  //         window.location.reload();
-  //       });
-  //     }
-  //   });
-  // }
-
-
-
   acceptMerchant(merchantId: string) {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "Do you want to accept this merchant?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, accept it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.authService.acceptMerchant(merchantId).subscribe({
-          next: (response) => {
-            console.log('Merchant accepted:', response);
-            Swal.fire(
-              'Accepted!',
-              'The merchant has been accepted.',
-              'success'
-            ).then(() => {
-              window.location.reload();
-            });
-          },
-          error: (error) => {
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Something went wrong!',
-            });
-          }
+    this.authService.acceptMerchant(merchantId).subscribe({
+      next: (response) => {
+        console.log('Merchant accepted:', response);
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+        });
+      },
+      complete: () => {
+        console.log('Accepting merchant complete.');
+        Swal.fire({
+          icon: 'success',
+          title: 'Merchant accepted!',
+          text: 'Merchant has been accepted.',
+        }).then(() => {
+          // Reload the page
+          window.location.reload();
         });
       }
     });
   }
-  
+
   rejectMerchant(merchantId: string) {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "Do you want to reject this merchant?",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Yes, reject it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.authService.rejectMerchant(merchantId).subscribe({
-          next: (response) => {
-            console.log('Merchant rejected:', response);
-            Swal.fire(
-              'Rejected!',
-              'The merchant has been rejected.',
-              'success'
-            ).then(() => {
-              window.location.reload();
-            });
-          },
-          error: (error) => {
-            Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Something went wrong!',
-            });
-          }
+    this.authService.rejectMerchant(merchantId).subscribe({
+      next: (response) => {
+        console.log('Merchant rejected:', response);
+      },
+      error: (error) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+        });
+      },
+      complete: () => {
+        console.log('Rejecting merchant complete.');
+        Swal.fire({
+          icon: 'success',
+          title: 'Merchant rejected!',
+          text: 'Merchant has been rejected.',
+        }).then(() => {
+          // Reload the page
+          window.location.reload();
         });
       }
     });
   }
-  
 }
+
