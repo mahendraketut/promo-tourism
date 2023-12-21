0fdddb53dddbf58d737d0180166807a6651a56ef
hendryar <hendrysuryadiarya@gmail.com>
2023-12-21 16:27:05 +0800

    checks for token expiry before being decoded

--- src/app/token.service.ts
+++ src/app/token.service.ts
@@ -4,12 +4,37 @@ import { jwtDecode } from "jwt-decode";
   providedIn: 'root',
 })
 export class TokenService {
+  //Gets a token from the local storage
   getToken(): string | null {
     return localStorage.getItem('token');
   }
+  //Removes token from local storage.
+  removeToken(): void {
+    localStorage.removeItem('token');
+  }
 
+  //Checks the token expiry, if its 5 seconds before expiring, we're good!
+  checkTokenExpiry(): boolean {
+    const token = this.getToken();
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const expiryDate = decodedToken.exp;
+      if (expiryDate) {
+        return 1000 * expiryDate - new Date().getTime() < 5000;
+      }
+    }
+    return true;
+  }
+
+  //Decodes the token if it exists.
+  //But, checks if the token is expired first. If it is, remove it.
+  //Hence, no token will be decoded.
   decodeToken(): any {
     const token = this.getToken();
+    if(this.checkTokenExpiry()){
+      this.removeToken();
+      return null;
+    }
     if (token) {
       try {
         return jwtDecode(token);
